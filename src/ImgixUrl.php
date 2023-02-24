e612c8b7f7e8d453b3820974e22015a39fd4882f
sfriel <G0g3t1t>
2023-02-24 10:38:22 -0500

    migrated to craft v4

--- src/ImgixUrl.php
+++ src/ImgixUrl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Imgix Url plugin for Craft CMS 3.x
  *
@@ -59,12 +60,12 @@ class ImgixUrl extends Plugin
      *
      * @var string
      */
-    public $schemaVersion = '3.0.0';
+    public string $schemaVersion = '3.0.0';
 
     /**
      * Check if imageoptimize is installed.
      *
-     * @var string
+     * @var bool
      */
     public $isImageOptimizedInstalled;
 
@@ -82,12 +83,16 @@ class ImgixUrl extends Plugin
      * you do not need to load it in your init() method.
      *
      */
-    public function init()
+    public function init(): void
     {
         parent::init();
         self::$plugin = $this;
 
-        $this->isImageOptimizedInstalled = Craft::$app->plugins->isPluginInstalled('image-optimize');
+        /**
+         * @var \craft\services\Plugins $pluginsService
+         */
+        $pluginsService = Craft::$app->getPlugins();
+        $this->isImageOptimizedInstalled = $pluginsService->isPluginInstalled('image-optimize');
 
         // Register our variables
         Event::on(
@@ -111,24 +116,24 @@ class ImgixUrl extends Plugin
             }
         );
 
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
+        /**
+         * Logging in Craft involves using one of the following methods:
+         *
+         * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+         * Craft::info(): record a message that conveys some useful information.
+         * Craft::warning(): record a warning message that indicates something unexpected has happened.
+         * Craft::error(): record a fatal error that should be investigated as soon as possible.
+         *
+         * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+         *
+         * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+         * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+         *
+         * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+         * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+         *
+         * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
+         */
         Craft::info(
             Craft::t(
                 'imgix-url',
@@ -146,7 +151,7 @@ class ImgixUrl extends Plugin
      *
      * @return \craft\base\Model|null
      */
-    protected function createSettingsModel()
+    protected function createSettingsModel(): ?\craft\base\Model
     {
         return new Settings();
     }
--- src/services/ImgixUrlService.php
+++ src/services/ImgixUrlService.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Imgix Url plugin for Craft CMS 3.x
  *
@@ -10,10 +11,17 @@
 
 namespace sprokets\imgixurl\services;
 
-use sprokets\imgixurl\ImgixUrl;
-
 use Craft;
+
+use Exception;
 use craft\base\Component;
+use craft\elements\Asset;
+use yii\web\HttpException;
+use craft\base\FsInterface;
+use sprokets\imgixurl\ImgixUrl;
+use yii\base\InvalidConfigException;
+use yii\db\Exception as DbException;
+use yii\base\InvalidArgumentException;
 
 /**
  * ImgixUrlService Service
@@ -30,86 +38,111 @@ use craft\base\Component;
  */
 class ImgixUrlService extends Component
 {
-    // Public Methods
-    // =========================================================================
+  // Public Methods
+  // =========================================================================
 
-    public function getRawAssetUrl($asset) {
-      if(!ImgixUrl::$plugin->isImageOptimizedInstalled) {
-          return $asset->getUrl();
+  /**
+   * 
+   * @param Asset $asset 
+   * @return string|null 
+   * @throws InvalidConfigException 
+   */
+  public function getRawAssetUrl(Asset $asset): ?string
+  {
+    if (!ImgixUrl::$plugin->isImageOptimizedInstalled) {
+      return $asset->getUrl();
+    } else {
+      $baseUrl = "";
+      $fs = $asset->getVolume()->getFs();
+      if ($fs->hasUrls) {
+        $baseUrl = $fs->getRootUrl();
+      }
+      return $baseUrl . (substr($baseUrl, -1) == '/' ? '' : '/') . $asset->getPath();
+    }
+  }
+
+  /**
+   * 
+   * @param Asset|string $imgInput 
+   * @param array<mixed> $settings 
+   * @return string|null 
+   * @throws Exception 
+   * @throws InvalidConfigException 
+   * @throws HttpException 
+   * @throws DbException 
+   * @throws InvalidArgumentException 
+   */
+  public function getUrl(Asset|string $imgInput, array $settings = []): ?string
+  {
+
+    $img = $imgInput;
+    if (!is_string($imgInput)) {
+      /**
+       * @var \craft\services\Plugins $pluginsService
+       */
+      $pluginsService = Craft::$app->getPlugins();
+      $isImageOptimizedInstalled = $pluginsService->getPlugin('image-optimize');
+
+      if (!$isImageOptimizedInstalled) {
+        $img = $imgInput->getUrl();
+        if ($img === null) {
+          return null;
         }
-        else {
-          $vol = $asset->getVolume()->getRootUrl();
-          return $vol . (substr($vol , -1)=='/' ? '' : '/') . $asset->getPath();
+      } else {
+        $baseUrl = "";
+        $fs = $imgInput->getVolume()->getFs();
+        if ($fs->hasUrls) {
+          $baseUrl = $fs->getRootUrl();
+        }
+        $img = $baseUrl . (substr($baseUrl, -1) == '/' ? '' : '/') . $imgInput->getPath();
       }
     }
 
-    public function getUrl($imgInput, $settings=[]) {
+    $config = Craft::$app->config->getConfigFromFile('imgixurl');
+    $sources = $config['sources'];
 
-        $img = $imgInput;
-        if(!is_string($imgInput)) {
-          $isImageOptimizedInstalled = Craft::$app->plugins->getPlugin('image-optimize');
+    if (!is_array($sources)) {
 
-          if(!$isImageOptimizedInstalled) {
-            $img = $imgInput->getUrl();
-          }
-          else {
-            $vol = $imgInput->getVolume()->getRootUrl();
-            $img = $vol . (substr($vol , -1)=='/' ? '' : '/') . $imgInput->getPath();
-          }
-        }
-
-
-        $config = Craft::$app->config->getConfigFromFile('imgixurl');
-        $sources = $config['sources'];
-
-        if(!is_array($sources)) {
-
-          $transSettings = array();
-          if(isset($settings['w'])) {
-            $transSettings['width'] = $settings['w'];
-          }
-
-          if(isset($settings['h'])) {
-            $transSettings['height'] = $settings['h'];
-          }
-
-          return $imgInput->getUrl($transSettings);
-
-        }
-
-        $defaultSettings = $config['defaultSettings'];
-
-        if(!is_array($defaultSettings)) {
-          $defaultSettings = array();
-        }
-
-        $settingsString = http_build_query(array_merge($defaultSettings, $settings));
-
-        $filteredImg = explode('?', $img)[0];
-
-        $imgPath = '';
-        $i = 0;
-
-        while ($i < sizeof($sources) && $imgPath == '') {
-          $source = $sources[$i];
-
-          $pos = strpos($filteredImg, $source['original']);
-
-          if($pos !== false) {
-            $part = substr($filteredImg, $pos + strlen($source['original'])) . '?' . $settingsString;
-            $imgPath = $source['imgix'] . $part;
-
-            if(isset($source['token'])) {
-              $imgPath .= '&s=' . md5($source['token'] . $part);
-            }
-          }
-          $i++;
-        }
-
-
-
-
-        return empty($imgPath) ? $img : $imgPath;
-
+      $transSettings = array();
+      if (isset($settings['w'])) {
+        $transSettings['width'] = $settings['w'];
       }
+
+      if (isset($settings['h'])) {
+        $transSettings['height'] = $settings['h'];
+      }
+
+      return $imgInput->getUrl($transSettings);
+    }
+
+    $defaultSettings = $config['defaultSettings'];
+
+    if (!is_array($defaultSettings)) {
+      $defaultSettings = array();
+    }
+
+    $settingsString = http_build_query(array_merge($defaultSettings, $settings));
+
+    $filteredImg = explode('?', $img)[0];
+
+    $imgPath = '';
+    $i = 0;
+
+    while ($i < sizeof($sources) && $imgPath == '') {
+      $source = $sources[$i];
+
+      $pos = strpos($filteredImg, $source['original']);
+
+      if ($pos !== false) {
+        $part = substr($filteredImg, $pos + strlen($source['original'])) . '?' . $settingsString;
+        $imgPath = $source['imgix'] . $part;
+
+        if (isset($source['token'])) {
+          $imgPath .= '&s=' . md5($source['token'] . $part);
+        }
+      }
+      $i++;
+    }
+    return empty($imgPath) ? $img : $imgPath;
+  }
 }
